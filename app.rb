5be9be854399b9b32fdc3bba0960102d952760d0
Alice Brunel <alc.brunel@gmail.com>
2019-09-25 08:59:43 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,5 +2,13 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   # Write your code here!
-
+  get '/reversename/:name' do 
+    @name = params[:name].reverse
+  end
+  
+  get '/square/:number' do 
+    @number = params[:number].to_i
+    "#{Integer.sqrt(@number)}"
+  end
+  
 end
