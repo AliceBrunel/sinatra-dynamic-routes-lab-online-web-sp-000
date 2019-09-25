628a72763f4f37dd04bfb2200b5d561e433f549e
Alice Brunel <alc.brunel@gmail.com>
2019-09-25 09:55:35 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -13,8 +13,7 @@ class App < Sinatra::Base
   
   get '/say/:number/:phrase' do 
     @number = params[:number].to_i
-    @phrase = params[:phrase]
-    @number.times {"#{@phrase}"}
+    "@number"
   end
   
 end
