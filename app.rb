a5b1fbaa6b2e68d3079468e51a4cae5ec0dd0439
Alice Brunel <alc.brunel@gmail.com>
2019-09-25 10:35:58 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -17,6 +17,9 @@ class App < Sinatra::Base
     "#{@phrase}" * @number 
   end
   
-  get 
+  get '/say/:word1/:word2/:word3/:word4/:word5' do 
+    @phrase = params[:word1] + params[:word2] + params[:word3] params[:word4] params[:word5]z
+    "#{@phrase}."
+  end
   
 end
