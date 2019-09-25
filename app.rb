d964101d9612de2410a7fc82b2cb0e2c436ad509
Alice Brunel <alc.brunel@gmail.com>
2019-09-25 10:44:54 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -26,8 +26,9 @@ class App < Sinatra::Base
     @operation = params[:operation]
     @num1 = params[:number1]
     @num2 = params[:number2]
+    @result = @num1.send(@operation,@num2)
     
-    @num1.send(@operation,@num2)
+    "#{@result}"
   end
   
   
